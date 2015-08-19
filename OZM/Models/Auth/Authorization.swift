008c8b8485b396e1bdfe2f813660fb07d4689269
Semyon Novikov <me@sdfgh153.ru>
2015-08-19 17:56:42 +0500

    Прибрался в коде немножко, пофиксил тест.
    Сайнинг работает

--- OZM/Models/Auth/Authorization.swift
+++ OZM/Models/Auth/Authorization.swift
@@ -8,13 +8,28 @@
 
 import Foundation
 
+
+/**
+    Общий с сервером ключик для HMAC SHA256
+ */
+let sharedKey = "d5HjIGxYEnSH5dawbOutjOjgAWhGUlXBC6iNZnpI35eNJJpkIedp8cLuHtLeOPO1"
+
 /**
     Функция генерации подписи для запроса.
     Документация: https://basecamp.com/2954234/projects/9473082/documents/9374715
+
+    Я дальше немного попишу, чтобы не сойти с ума, когда придётся это менять.
+    
+    :param: url URL запроса
+    :param: body Тело запроса
+    :param: userKey "Публичный" ключ пользователя полученный при регистрации
+    :param: secret "Секретный" ключ пользователя
+    :param: timestamp Текущее время в Unixtime
  */
-public func sign(url: String, body: String, userKey: String, secret: String, timestamp: NSTimeInterval) -> String {
+public func signRequest(url: String, body: String, userKey: String, secret: String, timestamp: NSTimeInterval) -> String? {
+    /** Строка, которую будем подписывать */
     let concatenation = url + body + userKey + secret + "\(Int(timestamp))"
-    var key = "d5HjIGxYEnSH5dawbOutjOjgAWhGUlXBC6iNZnpI35eNJJpkIedp8cLuHtLeOPO1".dataUsingEncoding(
+    var key = sharedKey.dataUsingEncoding(
         NSUTF8StringEncoding,
         allowLossyConversion: false
     )
@@ -22,7 +37,7 @@ public func sign(url: String, body: String, userKey: String, secret: String, tim
         NSUTF8StringEncoding,
         allowLossyConversion: false
     )
-
+    /** Переменная, в которую будем складывать */
     var signatureOut = [CUnsignedChar](count: Int(CC_SHA256_DIGEST_LENGTH), repeatedValue: 0)
     if let key = key, let data = data {
         CCHmac(
@@ -33,9 +48,16 @@ public func sign(url: String, body: String, userKey: String, secret: String, tim
             data.length,
             &signatureOut
         )
-        let hash = NSData(bytes: signatureOut, length: Int(CC_SHA256_DIGEST_LENGTH))
-        let res = hash.base64EncodedDataWithOptions(.allZeros)
-        return String(NSString(data: res, encoding: NSUTF8StringEncoding)!)
+        let hashData = NSData(
+            bytes: signatureOut,
+            length: Int(CC_SHA256_DIGEST_LENGTH)
+        ).base64EncodedDataWithOptions(.allZeros)
+
+        if let hash = NSString(data: hashData, encoding: NSUTF8StringEncoding) {
+            return String(hash)
+        }
+        return nil
     }
-    return "didn't work :("
+    return nil
 }
+
--- OZMTests/OZMTests.swift
+++ OZMTests/OZMTests.swift
@@ -13,13 +13,13 @@ import OZM
 class HMACTest: XCTestCase {
 
     func testHmac() {
-        let hmac = sign(
+        let hmac = signRequest(
             "/authentication/register/",
             "{\"deviceId\":\"1235123\"}",
             "mtFxlt3JsW4D5wOl",
             "mu4C3KOi5zhqeMz7xAzkYT0lmrAeXy8JhMtEpd9ln6O7T8dN1aEm4lEY7xLtWqid",
-            1433878667)
-        println(hmac)
+            1433878667
+        )
         XCTAssert(hmac == "cuGDC6wM+/hAx1tnBwM2q0+Vh9I3o+WKRK2Ihe1fRkg=")
     }
 }
