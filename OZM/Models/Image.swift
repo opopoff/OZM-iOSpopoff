8ccac290a3b3bc2a079b49693db462624ec9d04f
Semyon Novikov <me@sdfgh153.ru>
2015-11-10 12:24:09 +0500

    Dropped useless code

--- OZM/Models/Image.swift
+++ OZM/Models/Image.swift
@@ -55,11 +55,12 @@ public struct Image: Deserializable {
     public func thumbSize() -> CGSize? {
         if let
             width = thumbnailWidth,
-            height = thumbnailHeight {
-                return CGSize(
-                    width: Double(width) / 2.0,
-                    height: Double(height) / 2.0
-                )
+            height = thumbnailHeight
+        {
+            return CGSize(
+                width: Double(width) / 2.0,
+                height: Double(height) / 2.0
+            )
         }
         return nil
     }
