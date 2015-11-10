f67beb34b0617172701f6cc98e04e90fa7f671fc
Semyon Novikov <me@sdfgh153.ru>
2015-11-10 13:40:37 +0500

    Stoopid sharing

--- OZM/Controllers/ImageController.swift
+++ OZM/Controllers/ImageController.swift
@@ -9,10 +9,12 @@
 import Foundation
 import UIKit
 
-class ImageController: UIViewController {
+class ImageController: UIViewController, UIDocumentInteractionControllerDelegate {
 
     let image: Image
     @IBOutlet weak var imageView: WebImageView!
+
+    var documentController: UIDocumentInteractionController!
 
     //MARK: - Initialization
     
@@ -25,10 +27,24 @@ class ImageController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func setupDocumentController(image: Image, data: NSData) -> Void {
+        var ext = ".jpg"
+        if let isGif = image.isGIF where isGif == true {
+            ext = ".gif"
+        }
+        let savePath = NSHomeDirectory().stringByAppendingString("/tmp\(ext)")
+        data.writeToFile(savePath, atomically: true)
+        documentController = UIDocumentInteractionController(URL: NSURL.fileURLWithPath(savePath))
+        documentController.delegate = self
+        self.fakeShare(self)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.contentMode = UIViewContentMode.ScaleAspectFit
-        imageView.setImageFromUrl(image.url!)
+        imageView.setImageFromUrl(image.url!).then { data in
+            self.setupDocumentController(self.image, data: data)
+        }
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -38,10 +54,21 @@ class ImageController: UIViewController {
 
     @IBAction func fakeShare(sender: AnyObject) {
         APIClient.likeImage(self.image)
+        documentController.presentOptionsMenuFromRect(CGRect(), inView: self.view, animated: true)
     }
 
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         self.navigationController?.navigationBarHidden = true
+    }
+
+    // MARK: - Document interaction stuff
+
+    func documentInteractionControllerWillPresentOpenInMenu(controller: UIDocumentInteractionController) {
+        print("Trying!")
+    }
+
+    func documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController) -> UIViewController {
+        return self
     }
 }
